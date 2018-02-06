92efd75be2dd2d07b4485c2c331062e4b5a15f52
Nick Joslyn <nick.joslyn@my.simpson.edu>
2018-02-06 13:13:37 -0600

    Produces a new molecule. ROUGH version, but works. Need to test with multiple pdb files.

--- B.py
+++ B.py
@@ -37,9 +37,11 @@ for atom in singleMolecule.getAtoms():
 uniqueGrids = np.unique(scoreBank[:,:-1], axis = 0)
 uniqueScores = np.zeros([len(uniqueGrids),2], dtype = float)
 # Find Best
-# Getting workable version -- not optimized
 
+# Pretty sure numpy arrays are much more clever than double loops but
+# just getting workable version -- not optimized
 
+#Average scores for each grid location and atom type
 for row in scoreBank:
     for i in range(len(uniqueGrids)):
         if(np.all(row[:-1] == uniqueGrids[i])):
@@ -48,4 +50,24 @@ for row in scoreBank:
             uniqueScores[i,1] += 1
             uniqueScores[i,0] = temp/uniqueScores[i,1]
 
-print(uniqueScores)
+#Find best average for each grid location
+uniqueDatabase = np.concatenate((uniqueGrids, uniqueScores), axis = 1)
+uniqueGridLocation = np.unique(uniqueDatabase[:,1])
+#molGrid = np.empty(len(uniqueGridLocation), dtype = '<U100')
+gridContents = np.empty(len(uniqueGridLocation))
+molScore = np.zeros(len(uniqueGridLocation))
+newMolecule = np.column_stack((uniqueGridLocation, gridContents, molScore))
+
+# Test molecule has both HD and NA at grid 0,2,0
+
+for gridspot in range(len(uniqueGridLocation)):
+    for i in range(len(uniqueDatabase[:,1])):
+        if (newMolecule[gridspot,0] == uniqueDatabase[i,1]):
+            #This is where a threshold could be
+            if(float(uniqueDatabase[i,2]) > float(newMolecule[gridspot,2])):
+                newMolecule[gridspot, 1] = uniqueDatabase[i,0]
+                newMolecule[gridspot, 2] = float(uniqueDatabase[i,2])
+## Need to compare scores and append the molecule type!!!!!
+
+print(newMolecule)
+print(len(newMolecule[:,1]))
