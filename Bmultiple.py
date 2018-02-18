73f0cd53305fa7fdb45df318a11562888f7318c2
Nick Joslyn <nick.joslyn@my.simpson.edu>
2018-02-18 14:22:30 -0600

    Cleaned up and documented Bmultiple.py -- Could still be optimized, but easy to follow.

--- Bmultiple.py
+++ Bmultiple.py
@@ -1,60 +1,37 @@
-# Step B Goal: average each grid position by atom type & molecule score
-# Process:
-#   Loop through list of molecules/PDB files
-#   Initialize each as a molecule object
-#   maxScore ; maxAtom ; gridLocation
-#   For grid in molecule.grid, for grid.atomType, average score and store in variables
-#   Return best atom for given grid location
+## Step B Goal: Average scores -- Find best atom type for each grid location
+## Process:
+##  1) Prepare list of input molecules
+##  2) Databse of all atoms: type, grid location, and score
+##  3) Database of unique grid/atom type combinations
+##  4) Array of best atom type for each grid location
+##  5) Threshold removes atoms/grids with low scores
 
 # Could put as a method in new class of molecules (MoleculeFiles)
 #--------------------------------------------------
-## Whole thing is rough draft
-
-#Molecules in Fixtures Directory
-#molecule3 -- 100
-#molecule4956 -- 130
-#molecule5390 -- 144
-#molecule5570 -- 101
-
-#For multiple:
 from Molecule import Molecule
+import numpy as np
+thresholdScore = 135
 
-listOfMolecules = []
-#MoleculeList-------------
-filename1 = "Fixtures/molecule03.pdb"
-pdbfile1 = open(filename1, "r")
-file1Score = 100
-molecule1 = Molecule('pdb', pdbfile1, file1Score, 0.75, 18, -35, -18)
-listOfMolecules.append(molecule1)
-
-filename2 = "Fixtures/molecule4956.pdb"
-pdbfile2 = open(filename2, "r")
-file2Score = 130
-molecule2 = Molecule('pdb', pdbfile2, file2Score, 0.75, 18, -35, -18)
-listOfMolecules.append(molecule2)
-
-filename3 = "Fixtures/molecule5390.pdb"
-pdbfile3 = open(filename3, "r")
-file3Score = 144
-molecule3 = Molecule('pdb', pdbfile3, file3Score, 0.75, 18, -35, -18)
-listOfMolecules.append(molecule3)
-
-filename4 = "Fixtures/molecule5570.pdb"
-pdbfile4 = open(filename4, "r")
-file4Score = 101
-molecule4 = Molecule('pdb', pdbfile4, file4Score, 0.75, 18, -35, -18)
-listOfMolecules.append(molecule4)
 #-------------------------
+# 1) Initialize and Populate listOfMolecules with correct pdb files/scores
+listOfMolecules = []
+filename1 = "Fixtures/molecule03.pdb"
+filename2 = "Fixtures/molecule4956.pdb"
+filename3 = "Fixtures/molecule5390.pdb"
+filename4 = "Fixtures/molecule5570.pdb"
+files = [filename1, 100, filename2, 130, filename3, 144, filename4, 101]
+
+for i in range(len(files)//2):
+    listOfMolecules.append(Molecule('pdb', open(files[2*i], "r"), files[2*i+1], 0.75, 18, -35, -18))
+#-------------------------
+
+#-------------------------
+# 2) Initialize and populate scoreBank (rows = atom, columns = type, grid, score)
 totalAtomCount = 0
 for i in listOfMolecules:
     totalAtomCount += i.getAtomCount()
-    print(i.getAtomCount())
-
-
-import numpy as np
 scoreBank = np.empty([totalAtomCount,3], dtype='<U100')
 
-##Run this on all molecules
 counter = 0
 for singleMolecule in listOfMolecules:
     currentScore = singleMolecule.getScore()
@@ -63,46 +40,41 @@ for singleMolecule in listOfMolecules:
         scoreBank[counter,1] = str(atom.getGrid().getX()) + " " + str(atom.getGrid().getY()) + " " + str(atom.getGrid().getZ())
         scoreBank[counter,2] = currentScore
         counter += 1
+#-------------------------
 
-#-------
+#-------------------------
+# 3) Initialize and populate atom type grid locations w/ averages
 uniqueGrids = np.unique(scoreBank[:,:-1], axis = 0)
 uniqueScores = np.zeros([len(uniqueGrids),2], dtype = float)
-# Find Best
 
-
-#Average scores for each grid location and atom type
 for row in scoreBank:
     for i in range(len(uniqueGrids)):
         if(np.all(row[:-1] == uniqueGrids[i])):
-            ##Average Score here
             temp = (uniqueScores[i,0] * uniqueScores[i,1]) + float(row[2])
             uniqueScores[i,1] += 1
             uniqueScores[i,0] = temp/uniqueScores[i,1]
+uniqueDatabase = np.column_stack((uniqueGrids, uniqueScores))
+#-------------------------
 
-#Find best average for each grid location
-uniqueDatabase = np.concatenate((uniqueGrids, uniqueScores), axis = 1)
+#-------------------------
+# 4) Initialize and populate new molecule -- best score at each grid location
 uniqueGridLocation = np.unique(uniqueDatabase[:,1])
-#molGrid = np.empty(len(uniqueGridLocation), dtype = '<U100')
 gridContents = np.empty(len(uniqueGridLocation))
 molScore = np.zeros(len(uniqueGridLocation))
 newMolecule = np.column_stack((uniqueGridLocation, gridContents, molScore))
 
-# Test molecule has both HD and NA at grid 0,2,0
-
 for gridspot in range(len(uniqueGridLocation)):
     for i in range(len(uniqueDatabase[:,1])):
         if (newMolecule[gridspot,0] == uniqueDatabase[i,1]):
-            #Possibly where a threshold could be
             if(float(uniqueDatabase[i,2]) > float(newMolecule[gridspot,2])):
                 newMolecule[gridspot, 1] = uniqueDatabase[i,0]
                 newMolecule[gridspot, 2] = float(uniqueDatabase[i,2])
-## Need to compare scores and append the molecule type!!!!!
+#-------------------------
 
-##Another possible way to do threshold:
-thresholdScore = 135
-#need astype b/c scores are strings right now
+#-------------------------
+# 5) Delete grids with atoms below threshold
 finalMolecule = np.delete(newMolecule, np.where(newMolecule[:, 2].astype(float)<thresholdScore), axis = 0)
-
+#-------------------------
 print(newMolecule)
 print(len(newMolecule[:,1]))
 print("HI")
