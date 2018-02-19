df9351feb1a0ed2a51382299a89e15a65e41dee4
Nick Joslyn <nick.joslyn@my.simpson.edu>
2018-02-18 20:01:16 -0600

    Step C in functions -- convert and immediate write capabilities

--- C.py
+++ C.py
@@ -1,115 +1,98 @@
 #Step C: Produce pdb file
-# In current form, I have all of stpe B from lines 10 - 65
-# True step C is only about 40 lines
-# Make sure that the first appearance of element name is not OA, HD, etc
+# One explicitly writes, one just converts
 
-#open file
-writeOutFile = open("testPDB.pdb", "w")
-gridSize = 0.75
-originX = 18
-originY = -35
-originZ = -18
+def writeToPDB(writeOutLocation, moleculeArray, gridSize, originX, originY, originZ):
+    writeOutFile = open(writeOutLocation, "w")
+    ATOM = "ATOM"
+    alternateIndicator = " "
+    residueName = "LIG"
+    chainIdentifier = " "
+    residueSequence = "1".rjust(4)
+    residueInsertions = " "
+    occupancy = "0".rjust(6)
+    temperatureFactor = "0".rjust(6)
+    segmentIdentifier = "0".ljust(4)
 
-#_______________________________
-# from step B -- need an input new molecule
+    pdbCounter = 1
+    for row in moleculeArray:
+        serialNumber = str(pdbCounter).rjust(5)
+        atomName = row[1]
+        if (atomName == 'NA'):
+            atomName = 'N'.ljust(4)
+        elif (atomName == 'HD'):
+            atomName = 'H'.ljust(4)
+        elif (atomName == 'OA'):
+            atomName = 'O'.ljust(4)
+        else:
+            atomName = atomName.ljust(4)
 
-from Molecule import Molecule
-import numpy as np
-thresholdScore = 135
+        coords = row[0].split()
+        if (float(coords[0]) < 0):
+            xCoord = str(float(coords[0]) * gridSize + originX - 0.5 * gridSize).rjust(8)
+        else:
+            xCoord = str(float(coords[0]) * gridSize + originX + 0.5 * gridSize).rjust(8)
+        if (float(coords[1]) < 0):
+            yCoord = str(float(coords[1]) * gridSize + originY - 0.5 * gridSize).rjust(8)
+        else:
+            yCoord = str(float(coords[1]) * gridSize + originY + 0.5 * gridSize).rjust(8)
+        if (float(coords[2]) < 0):
+            zCoord = str(float(coords[2]) * gridSize + originZ - 0.5 * gridSize).rjust(8)
+        else:
+            zCoord = str(float(coords[2]) * gridSize + originZ + 0.5 * gridSize).rjust(8)
 
-listOfMolecules = []
-filename1 = "Fixtures/molecule03.pdb"
-filename2 = "Fixtures/molecule4956.pdb"
-filename3 = "Fixtures/molecule5390.pdb"
-filename4 = "Fixtures/molecule5570.pdb"
-files = [filename1, 100, filename2, 130, filename3, 144, filename4, 101]
+        symbol = row[1].rjust(2)
 
-for i in range(len(files)//2):
-    listOfMolecules.append(Molecule('pdb', open(files[2*i], "r"), files[2*i+1], 0.75, 18, -35, -18))
+        lineInformation = ATOM + "  " + serialNumber + " " + atomName + alternateIndicator + residueName + " " + chainIdentifier + residueSequence + residueInsertions + "   " + xCoord + yCoord + zCoord + occupancy + temperatureFactor + "      " + segmentIdentifier + symbol + "\n"
+        writeOutFile.write(lineInformation)
 
-totalAtomCount = 0
-for i in listOfMolecules:
-    totalAtomCount += i.getAtomCount()
-scoreBank = np.empty([totalAtomCount,3], dtype='<U100')
+        pdbCounter += 1
 
-counter = 0
-for singleMolecule in listOfMolecules:
-    currentScore = singleMolecule.getScore()
-    for atom in singleMolecule.getAtoms():
-        scoreBank[counter,0] = str(atom.getAtomType())
-        scoreBank[counter,1] = str(atom.getGrid().getX()) + " " + str(atom.getGrid().getY()) + " " + str(atom.getGrid().getZ())
-        scoreBank[counter,2] = currentScore
-        counter += 1
+    writeOutFile.write("END")
+    writeOutFile.close()
+    return
 
-uniqueGrids = np.unique(scoreBank[:,:-1], axis = 0)
-uniqueScores = np.zeros([len(uniqueGrids),2], dtype = float)
+def convertToPDB(moleculeArray, gridSize, originX, originY, originZ):
+    ATOM = "ATOM"
+    alternateIndicator = " "
+    residueName = "LIG"
+    chainIdentifier = " "
+    residueSequence = "1".rjust(4)
+    residueInsertions = " "
+    occupancy = "0".rjust(6)
+    temperatureFactor = "0".rjust(6)
+    segmentIdentifier = "0".ljust(4)
 
-for row in scoreBank:
-    for i in range(len(uniqueGrids)):
-        if(np.all(row[:-1] == uniqueGrids[i])):
-            temp = (uniqueScores[i,0] * uniqueScores[i,1]) + float(row[2])
-            uniqueScores[i,1] += 1
-            uniqueScores[i,0] = temp/uniqueScores[i,1]
-uniqueDatabase = np.column_stack((uniqueGrids, uniqueScores))
+    lineInformation = []
+    pdbCounter = 1
+    for row in moleculeArray:
+        serialNumber = str(pdbCounter).rjust(5)
+        atomName = row[1]
+        if (atomName == 'NA'):
+            atomName = 'N'.ljust(4)
+        elif (atomName == 'HD'):
+            atomName = 'H'.ljust(4)
+        elif (atomName == 'OA'):
+            atomName = 'O'.ljust(4)
+        else:
+            atomName = atomName.ljust(4)
 
-uniqueGridLocation = np.unique(uniqueDatabase[:,1])
-gridContents = np.empty(len(uniqueGridLocation))
-molScore = np.zeros(len(uniqueGridLocation))
-newMolecule = np.column_stack((uniqueGridLocation, gridContents, molScore))
+        coords = row[0].split()
+        if (float(coords[0]) < 0):
+            xCoord = str(float(coords[0]) * gridSize + originX - 0.5 * gridSize).rjust(8)
+        else:
+            xCoord = str(float(coords[0]) * gridSize + originX + 0.5 * gridSize).rjust(8)
+        if (float(coords[1]) < 0):
+            yCoord = str(float(coords[1]) * gridSize + originY - 0.5 * gridSize).rjust(8)
+        else:
+            yCoord = str(float(coords[1]) * gridSize + originY + 0.5 * gridSize).rjust(8)
+        if (float(coords[2]) < 0):
+            zCoord = str(float(coords[2]) * gridSize + originZ - 0.5 * gridSize).rjust(8)
+        else:
+            zCoord = str(float(coords[2]) * gridSize + originZ + 0.5 * gridSize).rjust(8)
 
-for gridspot in range(len(uniqueGridLocation)):
-    for i in range(len(uniqueDatabase[:,1])):
-        if (newMolecule[gridspot,0] == uniqueDatabase[i,1]):
-            if(float(uniqueDatabase[i,2]) > float(newMolecule[gridspot,2])):
-                newMolecule[gridspot, 1] = uniqueDatabase[i,0]
-                newMolecule[gridspot, 2] = float(uniqueDatabase[i,2])
+        symbol = row[1].rjust(2)
 
-finalMolecule = np.delete(newMolecule, np.where(newMolecule[:, 2].astype(float)<thresholdScore), axis = 0)
-#_______________________________
+        lineInformation.append(ATOM + "  " + serialNumber + " " + atomName + alternateIndicator + residueName + " " + chainIdentifier + residueSequence + residueInsertions + "   " + xCoord + yCoord + zCoord + occupancy + temperatureFactor + "      " + segmentIdentifier + symbol + "\n")
+        pdbCounter += 1
 
-ATOM = "ATOM"
-alternateIndicator = " "
-residueName = "LIG"
-chainIdentifier = " "
-residueSequence = "1".rjust(4)
-residueInsertions = " "
-occupancy = "0".rjust(6)
-temperatureFactor = "0".rjust(6)
-segmentIdentifier = "0".ljust(4)
-
-pdbCounter = 1
-for row in finalMolecule:
-    serialNumber = str(pdbCounter).rjust(5)
-    atomName = row[1]
-    if (atomName == 'NA'):
-        atomName = 'N'.ljust(4)
-    elif (atomName == 'HD'):
-        atomName = 'H'.ljust(4)
-    elif (atomName == 'OA'):
-        atomName = 'O'.ljust(4)
-    else:
-        atomName = atomName.ljust(4)
-
-    coords = row[0].split()
-    if (float(coords[0]) < 0):
-        xCoord = str(float(coords[0]) * gridSize + originX - 0.5 * gridSize).rjust(8)
-    else:
-        xCoord = str(float(coords[0]) * gridSize + originX + 0.5 * gridSize).rjust(8)
-    if (float(coords[1]) < 0):
-        yCoord = str(float(coords[1]) * gridSize + originY - 0.5 * gridSize).rjust(8)
-    else:
-        yCoord = str(float(coords[1]) * gridSize + originY + 0.5 * gridSize).rjust(8)
-    if (float(coords[2]) < 0):
-        zCoord = str(float(coords[2]) * gridSize + originZ - 0.5 * gridSize).rjust(8)
-    else:
-        zCoord = str(float(coords[2]) * gridSize + originZ + 0.5 * gridSize).rjust(8)
-
-    symbol = row[1].rjust(2)
-
-    lineInformation = ATOM + "  " + serialNumber + " " + atomName + alternateIndicator + residueName + " " + chainIdentifier + residueSequence + residueInsertions + "   " + xCoord + yCoord + zCoord + occupancy + temperatureFactor + "      " + segmentIdentifier + symbol + "\n"
-    writeOutFile.write(lineInformation)
-
-    pdbCounter += 1
-
-writeOutFile.write("END")
-writeOutFile.close()
+    return lineInformation
